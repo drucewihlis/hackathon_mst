5657889ba4052ae5d14c3c8fcd763e5ef062d9b6
Vladimir Troshin <v.troshin@qsoft.ru>
2021-12-04 21:04:30 +0300

    Ачивки и отправка сообщений

---
+++ gmail.py
@@ -0,0 +1,31 @@
+import random
+
+
+def send_email(recipient):
+    import smtplib
+
+    subject = 'Auth code'
+    pwd = 'ZshilBylPes'
+    user = 'hackatonmoscow@gmail.com'
+    FROM = user
+    TO = recipient
+    SUBJECT = subject
+    TEXT = 'Your code ' + str(random.randint(100000, 999999))
+
+    # Prepare actual message
+    message = "\r\n".join([
+        "From: " + FROM,
+        "To: " + TO,
+        "Subject: " + SUBJECT,
+        "",
+        TEXT
+    ])
+    # try:
+    # SMTP_SSL Example
+    server_ssl = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+    server_ssl.ehlo()  # optional, called by login()
+    server_ssl.login(user, pwd)
+    # ssl server doesn't support or need tls, so don't call server_ssl.starttls()
+    server_ssl.sendmail(FROM, TO, message)
+    # server_ssl.quit()
+    server_ssl.close()
